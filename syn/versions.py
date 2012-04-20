d5795716f35ddaa5bcb8f551ce4ac8f35fb8803a
Paul Tagliamonte <tag@pault.ag>
2012-04-20 19:47:17 -0400

    Interesting....

--- syn/versions.py
+++ syn/versions.py
@@ -14,19 +14,22 @@ class Comparatron:
         return self._weights[self.string]
 
     def __lt__(self, other):
-        return self._get_weight < other._get_weight
+        return self._get_weight() < other._get_weight()
 
     def __gt__(self, other):
-        return self._get_weight > other._get_weight
+        return self._get_weight() > other._get_weight()
+
+    def __ne__(self, other):
+        return self._get_weight() != other._get_weight()
 
     def __eq__(self, other):
-        return self._get_weight == other._get_weight
+        return self._get_weight() == other._get_weight()
 
     def __ge__(self, other):
-        return self._get_weight >= other._get_weight
+        return self._get_weight() >= other._get_weight()
 
     def __le__(self, other):
-        return self._get_weight <= other._get_weight
+        return self._get_weight() <= other._get_weight()
 
 
 def _magic_strip(string, tokens):
@@ -42,26 +45,19 @@ def _magic_strip(string, tokens):
     ret.append(buf)
     return ret
 
-def _do_compare(vid1, vid2):
+
+def _do_compare(vid1, vid2, test):
     delims = [ "+", ".", "~" ]
-    vid1s = _magic_strip(vid1, delims)
-    vid2s = _magic_strip(vid2, delims)
+    v1s = _magic_strip(vid1, delims)
+    v2s = _magic_strip(vid2, delims)
 
-    for x in range(0, len(vid1s)):
-        if vid1s[x] != vid2s[x]:
-            return vid1s[x] > vid2s[x]
+    for i in range(0, len(v1s)):
+        if v1s[i] != v2s[i]:
+            return test(v1s[i], v2s[i])
+    return None
 
-class VersionBlock:
-    def __init__(self, version_string):
-        self._vs = version_string
 
-    def __get_vs(self):
-        return self._vs
-
-    def __gt__(self, other):
-        cp = _do_compare(self._vs, other.__get_vs)
-        return cp == 1
-
-class Version:
-    def __init__(self, version_string):
-        self._vs = version_string
+def _cmp_lt(vid1, vid2):
+    def _lt(obj1, obj2):
+        return obj1 < obj2
+    return _do_compare(vid1, vid2, _lt)
