a62973595635f9feb0485173e334744643a506e0
Paul Tagliamonte <tag@pault.ag>
2012-04-20 21:49:12 -0400

    Slow tweeks

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -2,73 +2,56 @@
 
 from syn.versions import cmp_lt, cmp_gt, cmp_eq
 
-def test_simple():
+
+def test_less_then():
     v1 = "1"
     v2 = "2"
     assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-
-
-def test_janked():
-    v1 = "1.0.1"
-    v2 = "1.0"
-    assert cmp_gt(v1, v2)
-
-def test_minor():
     v1 = "1.0"
     v2 = "1.1"
     assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-    v1 = "1.0"
-    v2 = "2.0"
-    assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-
-
-def test_patch():
     v1 = "1.0.0"
     v2 = "1.0.1"
     assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-    v1 = "1.0.0"
-    v2 = "1.1.0"
-    assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-    v1 = "1.0.0"
-    v2 = "2.0.0"
-    assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-
-
-def test_plusthing():
-    v1 = "1.0.0~1"
-    v2 = "1.0.0"
-    assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-    v1 = "1.0.0+1"
-    v2 = "1.0.0.0"
-    assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-    v1 = "1.0.0~1"
-    v2 = "1.0.0+1"
-    assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-    v1 = "1.0.0+1"
-    v2 = "1.0.0+2"
-    assert cmp_lt(v1, v2)
-    assert cmp_gt(v2, v1)
-
-
-def test_eqs():
-    v1 = "1"
-    v2 = "1"
-    assert cmp_eq(v1, v2)
-    v1 = "1.0"
+    v1 = "1~1"
     v2 = "1.0"
-    assert cmp_eq(v1, v2)
+    assert cmp_lt(v1, v2)
+    v1 = "1+0"
+    v2 = "1.1"
+    assert cmp_lt(v1, v2)
+    v1 = "1~1"
+    v2 = "1+1"
+    assert cmp_lt(v1, v2)
+    v1 = "1"
+    v2 = "1+1"
+    assert cmp_lt(v1, v2)
+    v1 = "1+1"
+    v2 = "1+2"
+    assert cmp_lt(v1, v2)
+
+
+def test_greater_then():
+    v1 = "1"
+    v2 = "2"
+    assert cmp_gt(v2, v1)
+    v1 = "1.0"
+    v2 = "1.1"
+    assert cmp_gt(v2, v1)
     v1 = "1.0.0"
-    v2 = "1.0.0"
-    assert cmp_eq(v1, v2)
-    v1 = "1.0.0+1~2"
-    v2 = "1.0.0+1~2"
-    assert cmp_eq(v1, v2)
+    v2 = "1.0.1"
+    assert cmp_gt(v2, v1)
+    v1 = "1~1"
+    v2 = "1.0"
+    assert cmp_gt(v2, v1)
+    v1 = "1+0"
+    v2 = "1.1"
+    assert cmp_gt(v2, v1)
+    v1 = "1~1"
+    v2 = "1+1"
+    assert cmp_gt(v2, v1)
+    v1 = "1"
+    v2 = "1+1"
+    assert cmp_gt(v2, v1)
+    v1 = "1+1"
+    v2 = "1+2"
+    assert cmp_gt(v2, v1)
