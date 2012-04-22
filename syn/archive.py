a756f7b5d6394435d65715360a76ea1c539745a2
Paul Tagliamonte <tag@pault.ag>
2012-04-22 13:20:00 -0400

    Adding in archive types

---
+++ syn/archive.py
@@ -0,0 +1,18 @@
+# Copyright
+
+
+class Tarball:
+    def __init__(self, file_path):
+        pass
+
+
+class UpstreamTarball(Tarball):
+    pass
+
+
+class SynSourceTarball(Tarball):
+    pass
+
+
+class SynBinaryTarball(Tarball):
+    pass
