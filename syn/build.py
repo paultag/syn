b8267c61431eedd84c7c3ba84eb3e3db85e1322d
Paul Tagliamonte <tag@pault.ag>
2012-04-21 22:12:24 -0400

    friggn'

--- syn/build.py
+++ syn/build.py
@@ -1,6 +1,7 @@
 # Copyright header
 
 from syn.system import cd, run, mkdir, putenv, abspath
+from syn.db import Database
 import os
 
 def preform_step(step_name):
@@ -9,7 +10,10 @@ def preform_step(step_name):
 
 def build_source_package(root, steps):
     with cd(root):
-        mkdir("synd/tmp")
+        metainf = Database("synd/metainf")
+        mkdir("synd/tmp", destroy_old=True)
         putenv("SYN_DESTDIR", abspath("synd/tmp"))
         for step in steps:
             preform_step(step)
+        # We've now got our tree (magically) in synd/tmp
+        # Let's package it nicely.
--- syn/system.py
+++ syn/system.py
@@ -3,6 +3,7 @@
 from contextlib import contextmanager
 import subprocess
 import os.path
+import shutil
 import os
 
 @contextmanager
@@ -19,9 +20,17 @@ def run(command):
     return subprocess.check_call(command)
 
 
-def mkdir(folder):
-    os.mkdir(folder)
+def mkdir(folder, destroy_old=False):
+    try:
+        os.mkdir(folder)
+    except OSError as e:
+        if e.errno == 17:
+            rmdir(folder)
+            return mkdir(folder, destroy_old=False)
 
+
+def rmdir(path):
+    shutil.rmtree(path)
 
 def abspath(folder):
     return os.path.abspath(folder)
