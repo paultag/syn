f86c60448bba5c8b7189ae8d8779030fd1760b17
Paul Tagliamonte <tag@pault.ag>
2012-04-21 21:53:53 -0400

    ideas, man.

--- syn/build.py
+++ syn/build.py
@@ -1,8 +1,15 @@
 # Copyright header
 
-from syn.system import cd
+from syn.system import cd, run, mkdir, putenv, abspath
 import os
 
-def build_source_package(root):
+def preform_step(step_name):
+    return run(["synd/build", step_name])
+
+
+def build_source_package(root, steps):
     with cd(root):
-        print os.getcwd()
+        mkdir("synd/tmp")
+        putenv("SYN_DESTDIR", abspath("synd/tmp"))
+        for step in steps:
+            preform_step(step)
--- syn/system.py
+++ syn/system.py
@@ -1,6 +1,8 @@
 # Copyright
 
 from contextlib import contextmanager
+import subprocess
+import os.path
 import os
 
 @contextmanager
@@ -11,3 +13,19 @@ def cd(path):
         yield
     finally:
         os.chdir(old_dir)
+
+
+def run(command):
+    return subprocess.check_call(command)
+
+
+def mkdir(folder):
+    os.mkdir(folder)
+
+
+def abspath(folder):
+    return os.path.abspath(folder)
+
+
+def putenv(key, value):
+    os.environ[key] = value
