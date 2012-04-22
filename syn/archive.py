6f18513e5becc26f653abfe7eb9309b9753d02ec
Paul Tagliamonte <tag@pault.ag>
2012-04-22 14:39:29 -0400

    archive shaz

--- syn/archive.py
+++ syn/archive.py
@@ -3,8 +3,31 @@
 
 class Tarball:
     def __init__(self, file_path):
+        self._process_filename(file_path)
+        self._fp = file_path
+        self._startup_hook()
+
+    def _process_filename(self, file_path):
         pass
 
+    def _startup_hook(self):
+        pass
+
+    def _extract_hook(self):
+        pass
+
+    def _checksum_hook(self):
+        pass
+
+    def extract_to(self, file_path):
+        self._extract_hook()
+        with tarfile.open(file_path, mode='r') as fd:
+            pass
+
+
+    def checksum(self):
+        self._checksum_hook()
+        pass
 
 class UpstreamTarball(Tarball):
     pass
